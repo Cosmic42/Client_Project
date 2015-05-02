daa7ba62db178186478e9a6cd2fa995b353e8a2c
Cosmic42 <niuwa11y@gmail.com>
2015-05-01 22:22:54 -0400

    Create an inventory object that is used to store the information of each book. Constructor will be worked on after formatting the database

---
+++ Client_Project/src/InventoryObject.java
@@ -0,0 +1,13 @@
+
+public class InventoryObject {
+	private String name;
+	private String grade;
+	private int total;
+	private int numOut;
+	private double price;
+	private String ISBN;
+	
+	public InventoryObject(){
+		
+	}
+}
